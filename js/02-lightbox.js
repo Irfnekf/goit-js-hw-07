d66c683a4e8e5a9a720a5d9ce69da9f9fa949977
Irfnekf <vladshkaraputa07@gmail.com>
2022-11-13 20:39:33 +0200

    homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const ref = document.querySelector(".gallery");
+
+const createElList = galleryItems.reduce(
+  (acc, { preview, original, description }) =>
+    acc +
+    `<li class="gallery"><a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a></li>`,
+  ""
+);
+ref.insertAdjacentHTML("beforeend", createElList);
+
+function onClickImg(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+ref.addEventListener("click", onClickImg);
